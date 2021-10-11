15b95ddda6bb0a13ae874c5b7f54ea1032ea260e
AIDAN MC NEA - STUDENT <G00370308@gmit.ie>
2021-10-11 14:49:56 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import React, { Component } from 'react';
 import Header from './Components/header';
 import Footer from './Components/footer';
 import Content from './Components/content';
+import Read from './Components/read';
+import Create from './Components/create';
 import { Navbar, Nav } from 'react-bootstrap';
 import {
   BrowserRouter as Router,
@@ -23,17 +25,17 @@ class App extends Component {
             <Navbar.Brand href="#home">Navbar</Navbar.Brand>
             <Nav className="me-auto">
               <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/footer">Footer</Nav.Link>
-              <Nav.Link href="/header">Header</Nav.Link>
+              <Nav.Link href="/read">Read</Nav.Link>
+              <Nav.Link href="/create">Create</Nav.Link>
             </Nav>
           </Navbar>
 
           <Switch>
-            <Route path="/header">
-              <Header />
+            <Route path="/read">
+              <Read></Read>
             </Route>
-            <Route path="/footer">
-              <Footer />
+            <Route path="/create">
+             <Create></Create>
             </Route>
             <Route path="/">
               <Content />
